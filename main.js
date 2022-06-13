d9e427b5646fa210d6afd58e1b49f585c0ba6f11
MarqAenonD <106565814+MarqAenonD@users.noreply.github.com>
2022-06-13 17:10:09 -0500

    Update and rename main_85.js to main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 menu_list_array = [ "Veg Margherita Pizza",                  
-                                          ]; //Add more Pizza item names
+                    "Fish Pizza",
+		    "Hawaiian Pizza"]; //Add more Pizza item names
 
 function getmenu()
 {
@@ -9,13 +10,13 @@ function getmenu()
         htmldata=htmldata+ menu_list_array[i] + '<br>'
     }
     //Give the appropriate id name as display_menu 
-    document.getElementById("         ").innerHTML = htmldata;
+    document.getElementById("display_menu").innerHTML = htmldata;
 }
 
 function add_suggestion(){
     //Give the appropriate id name as add_item
-	var item = document.getElementById("         ").value;
+	var item = document.getElementById("add_item").value;
     //Use the push() function to push the item into menu_list_array
-    menu_list_array.     (item);
+    menu_list_array.push(item);
 }
 
